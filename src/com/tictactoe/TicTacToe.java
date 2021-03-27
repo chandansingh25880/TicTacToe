f33bab0089101f38880110e6420bfbefd2113250
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-27 09:02:35 +0530

    Creating TicTacToe Board

---
+++ src/com/tictactoe/TicTacToe.java
@@ -0,0 +1,21 @@
+package com.tictactoe;
+public class TicTacToe {
+	public static char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToe");
+		initializeBoard();
+
+	}
+
+	public static void initializeBoard() {
+		System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+		System.out.println("-+-+-");
+	}
+
+
+}
