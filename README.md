408624554fe3b7d2a0186ba061f0647574e579ff
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-27 09:50:15 +0530

    Chhose only O and X

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-echo # TicTacToe
+# TicTacToe
--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -1,10 +1,14 @@
 package com.tictactoe;
+
+import java.util.Scanner;
+
 public class TicTacToe {
 	public static char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
 		initializeBoard();
+		chooseMove();
 
 	}
 
@@ -16,6 +20,27 @@ public class TicTacToe {
 		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
 		System.out.println("-+-+-");
 	}
+ public static void chooseMove()
+ {
+	 System.out.println("Enter your Move");
+	 Scanner sc=new Scanner(System.in);
+	 String Move=sc.nextLine();
+	 if(Move.equalsIgnoreCase("X") || Move.equalsIgnoreCase("O"))
+	 {
+		 System.out.println("Eneter Row Number :");
+		 int row =sc.nextInt();
+		 System.out.println("Enter column Number");
+		 int col = sc.nextInt();
+		 if (board[row - 1][col - 1] == 'X' || board[row - 1][col - 1] == 'O') {
+				System.out.println("The place is already is filled");
+				chooseMove();
+			} else
+				board[row - 1][col - 1] = Move.toUpperCase().toCharArray()[0];
+		} else {
+			System.out.println("Enter your valid move,either X or O");
+		}
+			 
+		 
+	 }
+ }
 
-
-}
