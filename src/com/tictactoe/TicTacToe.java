7fe70803e20a6f5b27a0c7dc882589ca135d60d6
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-30 23:26:53 +0530

    Player turn and Computer turn

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -2,54 +2,99 @@ package com.tictactoe;
 
 import java.util.Scanner;
 
-public class TicTacToe {
-	public static char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+	public class TicTacToe {
+		public static char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+		private static Scanner in = new Scanner(System.in);
+		private static Board board = new Board();
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to TicTacToe");
-		initializeBoard();
-		chooseMove();
-       
-	}
+		private static boolean gameEnded = false;
+		private static boolean player = playerturn();
+		private static boolean sessionEnded = false;
 
-	public static void initializeBoard() {
-		System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
-		System.out.println("-+-+-");
-		System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
-		System.out.println("-+-+-");
-		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
-		System.out.println("-+-+-");
-	}
- public static void chooseMove()
- {
-	 System.out.println("Enter your Move");
-	 Scanner sc=new Scanner(System.in);
-	 String Move=sc.nextLine();
-	 if(Move.equalsIgnoreCase("X") || Move.equalsIgnoreCase("O"))
-	 {
-		 System.out.println("Eneter Row Number :");
-		 int row =sc.nextInt();
-		 System.out.println("Enter column Number");
-		 int col = sc.nextInt();
-		 if (board[row - 1][col - 1] == 'X' || board[row - 1][col - 1] == 'O') {
-				System.out.println("The place is already is filled");
-				chooseMove();
-			} else
-				board[row - 1][col - 1] = Move.toUpperCase().toCharArray()[0];
-		} else {
-			System.out.println("Please Enter Valid Move O and X");
+		public static void main(String[] args) {
+			System.out.println("Welcome to TicTacToe");
+			initializeBoard();
+			chooseMove();
+			playerTurn();
+			displayBoard();
+			selectBoard();
 		}
- }
+			System.out.println(board);
+			while (!sessionEnded) {
+				while (!gameEnded) {
+					Position position = null;
+					if (player) {
+						position = makeMove();
+						board = new Board(board, position, PlayerSign.Cross);
+					}
+					player = !player;
+					System.out.println(board);
 
-	 public static void displayBoard() {
+		public static void initializeBoard() 
+		{
+			System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+			System.out.println("-+-+-");
+			System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+			System.out.println("-+-+-");
+			System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+			System.out.println("-+-+-");
+				}
+
+			}
+		}
+
+		public static void chooseMove() {
+			System.out.println("Enter your move:");
+			Scanner sc = new Scanner(System.in);
+			String Move = sc.nextLine();
+			if (Move.equalsIgnoreCase("X") || Move.equalsIgnoreCase("O")) {
+				System.out.println("Enter row number:");
+				int row = sc.nextInt();
+				System.out.println("Enter column number:");
+				int col = sc.nextInt();
+				if (board[row - 1][col - 1] == 'X' || board[row - 1][col - 1] == 'O') {
+					System.out.println("The place is already is filled");
+					chooseMove();
+				} else
+					board[row - 1][col - 1] = Move.toUpperCase().toCharArray()[0];
+		private static boolean playerturn() {
+			int turn = (int) (Math.random() * 10 % 2);
+			if (turn == 0) {
+				System.out.println("Player first");
+				return true;
+			} else {
+				System.out.println("Enter your valid move,either X or O");
+				System.out.println("Computer first");
+				return false;
+			}
+
+		}
+
+		public static void displayBoard() {
 			for (int i = 0; i < board.length; i++) {
 				for (int j = 0; j < board[i].length; j++) {
+					/*
+					 * if (j == board[i].length - 1) System.out.print(board[i][j]); else
+					 */
 					System.out.print(board[i][j] + "   ");
 				}
 				System.out.println();
+		public static Position makeMove() {
+			Position position = null;
+			while (true) {
+				System.out.print("Pick 1,2 or 3 for row: ");
+				int row = getColOrRow();
+				System.out.print("Pick 1,2 or 3 for column: ");
+				int column = getColOrRow();
+				position = new Position(column, row);
+				if (board.isMarked(position))
+					System.out.println("Already marked!");
+				else
+					break;
 			}
+			return position;
 		}
-	 public static void selectBoard() {
+		public static void selectBoard() {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Where would you like to play? (1-9)");
 			String userInput = sc.nextLine();
@@ -81,24 +126,105 @@ public class TicTacToe {
 					break;
 				case "9":
 					board[2][2] = 'X';
+
+		private static int getColOrRow() {
+			int ret = -1;
+			while (true) {
+				try {
+					ret = Integer.parseInt(in.nextLine());
+				} catch (NumberFormatException e) {
+				}
+				if (ret < 1 | ret > 3)
+					System.out.print("\nInvalid input. Please pick 1,2 or 3: ");
+				else
 					break;
 					default:
 						System.out.println(" ");
 					}
 			displayBoard();
+			}
+			return ret - 1;
+		}
+	}
 
-		 
-	 }
-	 public static boolean playerTurn() {
-			int turn = (int) Math.random()*2;
+		public static boolean playerTurn() {
+			int turn = (int) Math.random()*1+1;
 			if(turn==0) {
 				System.out.println("Your turn first");
+	final class Position {
+		private final int column;
+		private final int row;
+
+		public Position(int column, int row) {
+			this.column = column;
+			this.row = row;
+		}
+
+		public int getRow() {
+			return this.row;
+		}
+
+		public int getColumn() {
+			return this.column;
+		}
+	}
+
+	enum PlayerSign {
+		Cross, Circle
+	}
+
+	enum GameState {
+		Incomplete, CrossWin, CircleWin, Draw
+	}
+
+	class Board {
+		private char[][] board; // e = empty, x = cross, o = circle.
+
+		public Board() {
+			board = new char[3][3];
+			for (int y = 0; y < 3; y++)
+				for (int x = 0; x < 3; x++)
+					board[x][y] = 'e'; // Board initially empty
+		}
+
+		public Board(Board from, Position position, PlayerSign sign) {
+			board = new char[3][3];
+			for (int y = 0; y < 3; y++)
+				for (int x = 0; x < 3; x++)
+					board[x][y] = from.board[x][y];
+			board[position.getColumn()][position.getRow()] = sign == PlayerSign.Cross ? 'x' : 'o';
+		}
+
+		public ArrayList<Position> getFreePositions() {
+			ArrayList<Position> retArr = new ArrayList<Position>();
+			for (int y = 0; y < 3; y++)
+				for (int x = 0; x < 3; x++)
+					if (board[x][y] == 'e')
+						retArr.add(new Position(x, y));
+			return retArr;
+		}
+
+		public boolean isMarked(Position position) {
+			if (board[position.getColumn()][position.getRow()] != 'e')
 				return true;
 			}else {
-				System.out.println("computer turn first");
+				System.out.println("Computer turn first");
 				return false;
-			}
-	 }
-}
- 
+			return false;
+		}
 
+		public String toString() {
+			String retString = "\n";
+			for (int y = 0; y < 3; y++) {
+				for (int x = 0; x < 3; x++) {
+					if (board[x][y] == 'x' || board[x][y] == 'o')
+						retString += "[" + board[x][y] + "]";
+					else
+						retString += "[ ]";
+				}
+				retString += "\n";
+			}
+			return retString;
+		}
+
+	}
