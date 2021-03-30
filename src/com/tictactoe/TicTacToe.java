f07f0d9c256cc44c3aaf4c520e721fcd3eaffcb5
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-30 22:06:45 +0530

    Player Want to display the board

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -9,6 +9,7 @@ public class TicTacToe {
 		System.out.println("Welcome to TicTacToe");
 		initializeBoard();
 		chooseMove();
+		displayBoard();
        
 	}
 
@@ -40,4 +41,14 @@ public class TicTacToe {
 			System.out.println("Please Enter Valid Move O and X");
 		}
  }
+
+
+public static void displayBoard() {
+	for (int i = 0; i < board.length; i++) {
+		for (int j = 0; j < board[i].length; j++) {
+			System.out.print(board[i][j] + "   ");
+		}
+		System.out.println();
+	}
+}
 }
