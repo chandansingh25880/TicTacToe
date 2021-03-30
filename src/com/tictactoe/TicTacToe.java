bf1bacd840936a0aca0ed522df46d021f9d4b934
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-30 22:24:57 +0530

    check space and toss who play first

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -9,8 +9,6 @@ public class TicTacToe {
 		System.out.println("Welcome to TicTacToe");
 		initializeBoard();
 		chooseMove();
-		displayBoard();
-		selectBoard();
        
 	}
 
@@ -43,53 +41,64 @@ public class TicTacToe {
 		}
  }
 
-
-public static void displayBoard() {
-	for (int i = 0; i < board.length; i++) {
-		for (int j = 0; j < board[i].length; j++) {
-			System.out.print(board[i][j] + "   ");
-		}
-		System.out.println();
-	}
-}
-public static void selectBoard() {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Where would you like to play? (1-9)");
-	String userInput = sc.nextLine();
-	System.out.println(userInput);
-	switch(userInput) {
-		case "1":
-			board[0][0] = 'X';
-			break;
-		case "2":
-			board[0][1] = 'X';
-			break;
-		case "3":
-			board[0][2] = 'X';
-			break;
-		case "4":
-			board[1][0] = 'X';
-			break;
-		case "5":
-			board[1][1] = 'X';
-			break;
-		case "6":
-			board[1][2] = 'X';
-			break;
-		case "7":
-			board[2][0] = 'X';
-			break;
-		case "8":
-			board[2][1] = 'X';
-			break;
-		case "9":
-			board[2][2] = 'X';
-			break;
-			default:
-				System.out.println(" ");
+	 public static void displayBoard() {
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) {
+					System.out.print(board[i][j] + "   ");
+				}
+				System.out.println();
 			}
-	displayBoard();
+		}
+	 public static void selectBoard() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Where would you like to play? (1-9)");
+			String userInput = sc.nextLine();
+			System.out.println(userInput);
+			switch(userInput) {
+				case "1":
+					board[0][0] = 'X';
+					break;
+				case "2":
+					board[0][1] = 'X';
+					break;
+				case "3":
+					board[0][2] = 'X';
+					break;
+				case "4":
+					board[1][0] = 'X';
+					break;
+				case "5":
+					board[1][1] = 'X';
+					break;
+				case "6":
+					board[1][2] = 'X';
+					break;
+				case "7":
+					board[2][0] = 'X';
+					break;
+				case "8":
+					board[2][1] = 'X';
+					break;
+				case "9":
+					board[2][2] = 'X';
+					break;
+					default:
+						System.out.println(" ");
+					}
+			displayBoard();
 
+		 
+	 }
+	 public static boolean playerTurn() {
+			int turn = (int) Math.random()*2;
+			if(turn==0) {
+				System.out.println("Your turn first");
+				return true;
+			}else {
+				System.out.println("computer turn first");
+				return false;
+			}
+	 }
+}
  
-}
-}
+
